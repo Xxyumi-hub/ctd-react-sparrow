4498487434328a485d5440f2bf8045ea2d3ecceb
Jequel Wilkes <grace.wilkes@sharpspring.com>
2022-05-02 21:32:16 -0400

    lesson 7

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,16 +3,31 @@ import AddTodoForm from "./AddTodoForm";
 import TodoList from './TodoList';
 import './App.css';
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')) || []);
-  useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-  }, [todoList]);
-
-  return [todoList, setTodoList];
-}
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({data: {
+          todoList: []
+        }
+      });
+        
+      }, 2000);
+    }).then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    })
+  }, [])
+  
+  useEffect(() => {
+    if (isLoading === false) {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+
+    }
+  }, [todoList]);
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
@@ -26,7 +41,7 @@ function App() {
       <div className="App">
         <h1>Todo List</h1>
         <AddTodoForm onAddTodo={addTodo}/>
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+        {isLoading ? <p>Loading</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>}
       </div>
     </>
   );
